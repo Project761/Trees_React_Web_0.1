47a568d17028bacc431bc5aa357caba67877b89e
devArustu <devendra.arustu@gmail.com>
2026-01-09 11:20:17 +0530

    first push

--- src/App.tsx
+++ src/App.tsx
@@ -891,7 +891,7 @@ function App() {
           muted 
           playsInline
           className="absolute inset-0 w-full h-full object-cover z-0"
-        >
+        >   
           <source src="/background .mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
